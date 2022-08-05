56884d3dd0fa1bdbf8843d4317b650038a4acc32
taitohaga <taitohaga@gmail.com>
2022-08-05 19:21:28 +0900

    fix install.bat

--- install.bat
+++ install.bat
@@ -4,6 +4,7 @@ echo "Making backup of the previous vimfiles..."
 
 if exist "%HOMEPATH%\vimfiles" (
     copy "%HOMEPATH%\vimfiles" "%HOMEPATH%\vimfiles_old"
+    del "%HOMEPATH%/vimfiles"
 )
 
 mklink /D "%HOMEPATH%\vimfiles" ".\.vim"
